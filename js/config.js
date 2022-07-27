b67da25f2d12c3a500a9fc1a46f666085ebc0626
Vanessa Henson <vanessahenson@vanessas-air.lan>
2022-07-27 10:47:07 -0600

    Added the config file from cognito

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_2Zg3LURvM',
+        userPoolClientId: 'us-east-1:040464660539:userpool/us-east-1_2Zg3LURvM',
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
